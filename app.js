588cc3748ffde84806264e59a88a2d27b0a97226
Knoll <noll.kyle@gmail.com>
2018-02-25 16:44:38 -0600

    Refactor code

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ function mouseout(x) {
   x.style.background = "green";
 }
 
-function clickButton() {
+function changeCellColor(color) {
   var cells = document.getElementsByClassName('cell');
   for (var i = 0; i < cells.length; i++) {
-    cells[i].style.background = "lightgreen";
+    cells[i].style.background = color;
   }
 }
 
-var button = document.createElement("button");
-button.setAttribute('onclick', 'clickButton()');
-button.innerHTML = "Clear";
-document.getElementById("container").appendChild(button);
+var clearButton = document.createElement("button");
+clearButton.setAttribute('onclick', "changeCellColor('lightgreen')");
+clearButton.innerHTML = "Clear";
+document.getElementById("container").appendChild(clearButton);
 
-var gridWidth = 16;
-var gridHeight = 10;
+gridWidth = 8;
+gridHeight = 8;
 
 var div = [];
 for (var i = 0; i < gridHeight; i++) {
@@ -29,11 +29,6 @@ for (var i = 0; i < gridHeight; i++) {
   document.getElementById("container").appendChild(div[i]);
   for (var j = 0; j < gridWidth; j++) {
     div[i][j] = document.createElement("div");
-    div[i][j].style.display = "inline-block";
-    div[i][j].style.background = "lightgreen";
-    div[i][j].style.margin = "2px";
-    div[i][j].style.height = "30px";
-    div[i][j].style.width = "30px";
     div[i][j].setAttribute('onmouseover', "mouseover(this)");
     div[i][j].setAttribute('onmouseout', "mouseout(this)");
     div[i][j].setAttribute('class', "cell");
