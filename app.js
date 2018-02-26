0f3df143a5185a537142b264fec9d1b34a714465
Knoll <noll.kyle@gmail.com>
2018-02-26 00:23:33 -0600

    Add working clear button

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-for (y = 0; y < 16; y++) {
-  for (x = 0; x < 16; x++) {
-    let div = document.createElement("div");
-    div.style.width     = "20px";
-    div.style.height    = "20px";
-    div.style.float     = "left";
-    div.style.border    = "1px solid black";
-    div.style.boxSizing = "border-box";
-    document.getElementById("container").appendChild(div);
-    div.addEventListener('mouseover', changeColor);
+function drawGrid(size) {
+  for (y = 0; y < size; y++) {
+    for (x = 0; x < size; x++) {
+      let div = document.createElement("div");
+      let cellSize = 320 / size;
+      div.style.width     = cellSize + "px";
+      div.style.height    = cellSize + "px";
+      div.style.float     = "left";
+      div.style.border    = "1px solid black";
+      div.style.boxSizing = "border-box";
+      document.getElementById("container").appendChild(div);
+      div.addEventListener('mouseover', changeColor);
+    }
   }
 }
 
 function changeColor(e) {
   e.target.style.background = "black";
 }
+
+function deleteCells() {
+  let cells = document.getElementById("container");
+  while (cells.hasChildNodes()) {
+    cells.removeChild(cells.lastChild); 
+  }
+  let size = prompt("Input grid size:");
+  drawGrid(size);
+}
+
+drawGrid(16);
