59c1ca2b82522c1d4f0361b3e710bb1d05d80a06
knoll3 <noll.kyle@gmail.com>
2018-02-26 19:24:32 +0000

    Adding grid size drop down

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+drawGrid(16);
+
+//--- FUNCTION DEFINITIONS --- ??
+
 function drawGrid(size) {
   for (let y = 0; y < size; y++) {
     for (let x = 0; x < size; x++) {
@@ -32,4 +36,3 @@ function promptForSize() {
   drawGrid(size);  
 }
 
-drawGrid(16);
