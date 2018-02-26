0c797d3973e953cd1bb018b3703c8cfbdf626c79
Knoll <noll.kyle@gmail.com>
2018-02-26 00:05:26 -0600

    Add draw feature

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-for (i = 0; i < 16; i++) {
-  let div = document.createElement("div");
-  div.innerHTML = "X";
-  document.getElementById("container").appendChild(div);
+for (y = 0; y < 16; y++) {
+  for (x = 0; x < 16; x++) {
+    let div = document.createElement("div");
+    div.style.width     = "20px";
+    div.style.height    = "20px";
+    div.style.float     = "left";
+    div.style.border    = "1px solid black";
+    div.style.boxSizing = "border-box";
+    document.getElementById("container").appendChild(div);
+    div.addEventListener('mouseover', changeColor);
+  }
+}
+
+function changeColor(e) {
+  e.target.style.background = "black";
 }
